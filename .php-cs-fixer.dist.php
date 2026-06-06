78e9709554d66b1734ef94ef1bfa3a17fc9b3b15
Ádám Bálint <adam.balint@srg.hu>
2026-06-06 10:02:04 +0200

    chore: Install PHP-CS-Fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = new PhpCsFixer\Finder()
+    ->in(__DIR__)
+    ->exclude('var')
+    ->notPath([
+        'config/bundles.php',
+        'config/reference.php',
+    ]);
+
+return new PhpCsFixer\Config()
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder);
